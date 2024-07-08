07b54e9aaef6e8eadcbec5927b34f684a8360e45
RaquelSousa <raquel.sousa.wt@gmail.com>
2024-07-08 13:01:44 +0100

    social media bar created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-import Header from "@/components/header/header";
-import { Input } from "@/components/ui/input";
+import Services from "./(sections)/services/page";
 
 export default function Home() {
   return (
     <div>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-          asdadasdad
+          <Services />
         </div>
       </main>
     </div>
